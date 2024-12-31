882eabfceb8889dd9ff5291361f9eacf7bf78392
Umensalma <139151207+Umensalma@users.noreply.github.com>
2024-12-31 23:08:50 +0530

    Add files via upload

---
+++ volunteer-platform/src/main/java/com/example/volunteer_platform/model/Task.java
@@ -0,0 +1,90 @@
+package com.example.task.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tasks")
+public class Task {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int task_id;
+	private String title;
+    private String description;
+    private String location;
+    private String skills;
+    private LocalDate date;
+    private String time;
+    private String status;
+    
+    public Task() {
+    	
+    }
+	public Task(int task_id, String title, String description, String location, String skills, LocalDate date,
+			String time, String status) {
+		super();
+		this.task_id = task_id;
+		this.title = title;
+		this.description = description;
+		this.location = location;
+		this.skills = skills;
+		this.date = date;
+		this.time = time;
+		this.status = status;
+	}
+	public int getTask_id() {
+		return task_id;
+	}
+	public void setTask_id(int task_id) {
+		this.task_id = task_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getSkills() {
+		return skills;
+	}
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+    
+	
+	
+}
