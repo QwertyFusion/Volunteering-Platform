629f0450cce1517febf818fbca6d476212d25497
Rishi Banerjee <rishibanerjee10@gmail.com>
2024-12-31 22:29:53 +0530

    Initial commit

---
+++ volunteer-platform/src/main/java/com/example/volunteer_platform/model/User.java
@@ -0,0 +1,13 @@
+package com.example.volunteer_platform.model;
+
+public class User {
+    private int userID;
+    
+    public int getUserID() {
+        return userID;
+    }
+    
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+}
