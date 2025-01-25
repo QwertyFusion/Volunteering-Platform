84620b03ed4ed7db2e3b59a91d1786c2ca9e0b27
Rishi Banerjee <rishibanerjee10@gmail.com>
2025-01-26 04:24:03 +0530

    Implemented Registraion: Now users can be registered onto the platform

--- volunteer-platform/src/main/java/com/example/volunteer_platform/config/SecurityConfig.java
+++ volunteer-platform/src/main/java/com/example/volunteer_platform/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/home", "/index.html", "/login", "/signup/**").permitAll()
+                        .requestMatchers("/", "/home", "/index.html", "/login", "/signup/**", "/api/**", "/register/**").permitAll()
                         .requestMatchers("/org/**").hasRole("ORGANIZATION")
                         .requestMatchers("/vol/**").hasRole("VOLUNTEER")
                         .anyRequest().authenticated()
